99aa9128bd7fe42b3d20677327d48ec11a6d3381
EliasDerHai <eliashaider95@gmail.om>
2022-03-12 22:57:19 +0100

    Fix Repository bug

--- src/graphic-asset/graphic-asset.module.ts
+++ src/graphic-asset/graphic-asset.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GraphicAssetService } from './graphic-asset.service';
 import { GraphicAssetController } from './graphic-asset.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphicAsset } from './entities/graphic-asset.entity';
 
 @Module({
   controllers: [GraphicAssetController],
-  providers: [GraphicAssetService]
+  providers: [GraphicAssetService],
+  imports: [TypeOrmModule.forFeature([GraphicAsset])]
+
 })
 export class GraphicAssetModule {}
